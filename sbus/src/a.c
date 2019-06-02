61a8089f8e70748f3448a0fb663ab1db68b89fc8
ntegan1 <ntegan1@gmail.com>
2019-06-02 19:26:57 +0100

    sbus

---
+++ sbus/src/a.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "sbus.h"
+
+int main (int argc, char **argv) {
+
+}
---
+++ sbus/src/sbus.h
@@ -0,0 +1,8 @@
+////
+//	# Do stuff
+//
+//	## buf: 		size = purpose
+//	## channels:	size + purpose
+////
+
+void fillBuf 	(void *buf, uint8_t *channels);
